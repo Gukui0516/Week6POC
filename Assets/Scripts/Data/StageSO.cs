f623c4086ba50ffb2df2b390b20a8541ac4506af
jish0902-ux <jish0902@gmail.com>
2025-10-18 23:57:17 +0900

    fix - stageCollectionSO

--- Assets/Scripts/Data/StageSO.cs
+++ Assets/Scripts/Data/StageSO.cs
@@ -48,22 +48,3 @@ public class StageSO : ScriptableObject
     }
 
 }
-
-/// <summary>
-/// 스테이지 컬렉션
-/// </summary>
-[CreateAssetMenu(fileName = "StageCollection", menuName = "Game/Stage Collection")]
-public class StageCollectionSO : ScriptableObject
-{
-    public List<StageSO> stages = new List<StageSO>();
-
-    public StageSO GetStage(int stageId)
-    {
-        return stages.Find(s => s.stageId == stageId);
-    }
-
-    public int GetTotalStageCount()
-    {
-        return stages.Count;
-    }
-}
